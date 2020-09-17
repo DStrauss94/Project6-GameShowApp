ef670e507a74430c375352225f125e233ad3741f
Douglas Strauss <douglasstrauss94@gmail.com>
2020-09-16 21:10:01 -0700

    working on checkWin

--- js/app.js
+++ js/app.js
@@ -3,6 +3,8 @@ const phrase = document.getElementById('phrase');
 const buttonReset = document.querySelector('.btn__reset');
 var elementUl = document.querySelector('#phrase ul');
 buttonReset.style.cursor = "pointer";
+const hearts = document.querySelectorAll(".tries");
+
 
 let missed = 0;
 
@@ -59,24 +61,39 @@ addPhraseToDisplay(phraseArray);
 
 
 qwerty.addEventListener("click", (e) =>{
+    
     if(e.target.tagName === 'BUTTON'){
         const button = e.target;
-        button.className = 'chosen';
-        const result = checkLetter(button);
-    }
-});
+        button.classList.add("chosen");
+        // button.setAttribute("disabled", "chosen");, second way of disabling
+        button.disabled = true;
+        // console.log(button.textContent);
+        // console.log(checkLetter(button));
+        const match =  checkLetter(button);
+        if (match === null ){
+            hearts[missed].style.display = 'none';
+            missed++;
+            
+        }
 
+        
+    }
+    
+});
 
 const checkLetter = (letterButton => {
     const letters = elementUl.children;
-    console.log(letterButton);
+    
     let match = null;
 
     for(let i = 0 ; i < letters.length; i++){
+        
         //checks to see if letter chosen, matches with letter at that index 
         if(letterButton.textContent === letters[i].textContent.toLowerCase()){
-            
+            letters[i].classList.add("chosen");
+            match = letterButton.textContent;
         }
+        
 
     }
 
@@ -85,6 +102,24 @@ const checkLetter = (letterButton => {
 });
 
 
+const checkWin = ((check) =>  {
+   //if letters in "show" match the number of letters in "letters"
+   //then win shows
+   let lengthShow = document.querySelectorAll('.show').length;
+   let lengthLetters;
+
+
+
+  
+
+
+
+
+
+
+
+   // else, lose 
+});
 
 
 
